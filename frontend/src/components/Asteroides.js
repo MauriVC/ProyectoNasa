610e3f600d2abd5cafbc7bdfba4b6789da53799f
AxelF0 <165232589+AxelF0@users.noreply.github.com>
2024-10-06 04:23:16 -0400

    v3

--- frontend/src/components/Asteroides.js
+++ frontend/src/components/Asteroides.js
@@ -20,29 +20,23 @@ function Asteroides() {
 
   return (
     <div>
-      <h2>Asteroides Cercanos a la Tierra</h2>
+      <h2>Asteroides del Sistema Solar</h2>
       <ul>
         {asteroides.map(asteroide => (
           <li key={asteroide._id}>
-            <h3>{asteroide.name}</h3>
-            <p><strong>Tamaño:</strong> {asteroide.size_m} m</p>
-            <p><strong>Distancia más cercana:</strong> {asteroide.closest_approach_km} km</p>
-            <p><strong>Velocidad:</strong> {asteroide.speed_km_per_h} km/h</p>
-            <p><strong>Peligroso:</strong> {asteroide.potential_hazard ? 'Sí' : 'No'}</p>
-            <p><strong>Fecha de Aproximación:</strong> {asteroide.approach_date}</p>
-            <p><strong>Órbita:</strong></p>
-            <ul>
-              <li><strong>Semi-eje mayor (a):</strong> {asteroide.a} AU</li>
-              <li><strong>Excentricidad (e):</strong> {asteroide.e}</li>
-              <li><strong>Inclinación (i):</strong> {asteroide.i}°</li>
-              <li><strong>Longitud del nodo ascendente (Ω):</strong> {asteroide.om}°</li>
-              <li><strong>Argumento del perihelio (w):</strong> {asteroide.w}°</li>
-              <li><strong>Distancia del perihelio (q):</strong> {asteroide.q} AU</li>
-              <li><strong>Distancia del afelio (ad):</strong> {asteroide.ad} AU</li>
-              <li><strong>Período orbital (años):</strong> {asteroide.per_y} años</li>
-              <li><strong>Arco de datos (días):</strong> {asteroide.data_arc}</li>
-            </ul>
-            <p><strong>Información adicional:</strong> {asteroide.additional_info}</p>
+            <h3>{asteroide.full_name}</h3>
+            <p><strong>Semi-eje mayor (a):</strong> {asteroide.a} AU</p>
+            <p><strong>Excentricidad (e):</strong> {asteroide.e}</p>
+            <p><strong>Inclinación (i):</strong> {asteroide.i}°</p>
+            <p><strong>Longitud del Nodo Ascendente (om):</strong> {asteroide.om}°</p>
+            <p><strong>Argumento del Perihelio (w):</strong> {asteroide.w}°</p>
+            <p><strong>Distancia del perihelio (q):</strong> {asteroide.q} AU</p>
+            <p><strong>Distancia del afelio (ad):</strong> {asteroide.ad} AU</p>
+            <p><strong>Período Orbital (años):</strong> {asteroide.per_y}</p>
+            <p><strong>Arco de datos:</strong> {asteroide.data_arc} días</p>
+            <p><strong>Código de condición:</strong> {asteroide.condition_code}</p>
+            <p><strong>Número de observaciones utilizadas:</strong> {asteroide.n_obs_used}</p>
+            <p><strong>Magnitud absoluta (H):</strong> {asteroide.H}</p>
           </li>
         ))}
       </ul>
